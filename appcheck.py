3102de836289d83de00cbffbb678c7ee8accee20
karthickn10 <nkarthickkarthick57@gmail.com>
2025-07-21 18:13:48 +0530

    Update appcheck.py

--- appcheck.py
+++ appcheck.py
@@ -1,2 +1,29 @@
+import os
+import math
+import random  # unused
 
-print(hello world ddi)
+# def unused_func():
+#     print("This function is commented out")
+
+def AddNumbers(a,b):  # Wrong naming + no type annotations
+    temp = a+b  # No spaces
+    result = temp + 5
+    temp2 = 100  # unused variable
+    return result
+
+def another_function():
+	print("This line is tab-indented")  # tab used
+	print("Another line") 
+
+if False:
+    print("This block will never run")
+
+class myclass:  # class name not in PascalCase
+    def __init__(self):
+        self.Value=10  # bad spacing and casing
+
+def long_line():
+    print("This is a very long line that definitely exceeds one hundred characters for sure, which breaks style guides.")
+
+def unused_args(x, y, z):  # z is unused
+    return x + y
